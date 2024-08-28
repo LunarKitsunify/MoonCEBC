f36c39d80c58db3a9acecccde09eb860ae8780ea
LunarKitsunify <kitsunify@gmail.com>
2024-08-28 10:32:57 +0600

    Start work on full screen

--- MoonCEBC.js
+++ MoonCEBC.js
@@ -15,11 +15,368 @@
 // @run-at document-end
 // ==/UserScript==
 
+//#region  bcSDK Stuff
+var bcModSdk = (function () {
+  "use strict";
+  const o = "1.2.0";
+  function e(o) {
+    alert("Mod ERROR:\n" + o);
+    const e = new Error(o);
+    throw (console.error(e), e);
+  }
+  const t = new TextEncoder();
+  function n(o) {
+    return !!o && "object" == typeof o && !Array.isArray(o);
+  }
+  function r(o) {
+    const e = new Set();
+    return o.filter((o) => !e.has(o) && e.add(o));
+  }
+  const i = new Map(),
+    a = new Set();
+  function c(o) {
+    a.has(o) || (a.add(o), console.warn(o));
+  }
+  function s(o) {
+    const e = [],
+      t = new Map(),
+      n = new Set();
+    for (const r of f.values()) {
+      const i = r.patching.get(o.name);
+      if (i) {
+        e.push(...i.hooks);
+        for (const [e, a] of i.patches.entries())
+          t.has(e) &&
+            t.get(e) !== a &&
+            c(
+              `ModSDK: Mod '${r.name}' is patching function ${
+                o.name
+              } with same pattern that is already applied by different mod, but with different pattern:\nPattern:\n${e}\nPatch1:\n${
+                t.get(e) || ""
+              }\nPatch2:\n${a}`
+            ),
+            t.set(e, a),
+            n.add(r.name);
+      }
+    }
+    e.sort((o, e) => e.priority - o.priority);
+    const r = (function (o, e) {
+      if (0 === e.size) return o;
+      let t = o.toString().replaceAll("\r\n", "\n");
+      for (const [n, r] of e.entries())
+        t.includes(n) ||
+          c(`ModSDK: Patching ${o.name}: Patch ${n} not applied`),
+          (t = t.replaceAll(n, r));
+      return (0, eval)(`(${t})`);
+    })(o.original, t);
+    let i = function (e) {
+      var t, i;
+      const a =
+          null === (i = (t = m.errorReporterHooks).hookChainExit) ||
+          void 0 === i
+            ? void 0
+            : i.call(t, o.name, n),
+        c = r.apply(this, e);
+      return null == a || a(), c;
+    };
+    for (let t = e.length - 1; t >= 0; t--) {
+      const n = e[t],
+        r = i;
+      i = function (e) {
+        var t, i;
+        const a =
+            null === (i = (t = m.errorReporterHooks).hookEnter) || void 0 === i
+              ? void 0
+              : i.call(t, o.name, n.mod),
+          c = n.hook.apply(this, [
+            e,
+            (o) => {
+              if (1 !== arguments.length || !Array.isArray(e))
+                throw new Error(
+                  `Mod ${
+                    n.mod
+                  } failed to call next hook: Expected args to be array, got ${typeof o}`
+                );
+              return r.call(this, o);
+            },
+          ]);
+        return null == a || a(), c;
+      };
+    }
+    return { hooks: e, patches: t, patchesSources: n, enter: i, final: r };
+  }
+  function l(o, e = !1) {
+    let r = i.get(o);
+    if (r) e && (r.precomputed = s(r));
+    else {
+      let e = window;
+      const a = o.split(".");
+      for (let t = 0; t < a.length - 1; t++)
+        if (((e = e[a[t]]), !n(e)))
+          throw new Error(
+            `ModSDK: Function ${o} to be patched not found; ${a
+              .slice(0, t + 1)
+              .join(".")} is not object`
+          );
+      const c = e[a[a.length - 1]];
+      if ("function" != typeof c)
+        throw new Error(`ModSDK: Function ${o} to be patched not found`);
+      const l = (function (o) {
+          let e = -1;
+          for (const n of t.encode(o)) {
+            let o = 255 & (e ^ n);
+            for (let e = 0; e < 8; e++)
+              o = 1 & o ? -306674912 ^ (o >>> 1) : o >>> 1;
+            e = (e >>> 8) ^ o;
+          }
+          return ((-1 ^ e) >>> 0).toString(16).padStart(8, "0").toUpperCase();
+        })(c.toString().replaceAll("\r\n", "\n")),
+        d = { name: o, original: c, originalHash: l };
+      (r = Object.assign(Object.assign({}, d), {
+        precomputed: s(d),
+        router: () => {},
+        context: e,
+        contextProperty: a[a.length - 1],
+      })),
+        (r.router = (function (o) {
+          return function (...e) {
+            return o.precomputed.enter.apply(this, [e]);
+          };
+        })(r)),
+        i.set(o, r),
+        (e[r.contextProperty] = r.router);
+    }
+    return r;
+  }
+  function d() {
+    for (const o of i.values()) o.precomputed = s(o);
+  }
+  function p() {
+    const o = new Map();
+    for (const [e, t] of i)
+      o.set(e, {
+        name: e,
+        original: t.original,
+        originalHash: t.originalHash,
+        sdkEntrypoint: t.router,
+        currentEntrypoint: t.context[t.contextProperty],
+        hookedByMods: r(t.precomputed.hooks.map((o) => o.mod)),
+        patchedByMods: Array.from(t.precomputed.patchesSources),
+      });
+    return o;
+  }
+  const f = new Map();
+  function u(o) {
+    f.get(o.name) !== o &&
+      e(`Failed to unload mod '${o.name}': Not registered`),
+      f.delete(o.name),
+      (o.loaded = !1),
+      d();
+  }
+  function g(o, t) {
+    (o && "object" == typeof o) ||
+      e("Failed to register mod: Expected info object, got " + typeof o),
+      ("string" == typeof o.name && o.name) ||
+        e(
+          "Failed to register mod: Expected name to be non-empty string, got " +
+            typeof o.name
+        );
+    let r = `'${o.name}'`;
+    ("string" == typeof o.fullName && o.fullName) ||
+      e(
+        `Failed to register mod ${r}: Expected fullName to be non-empty string, got ${typeof o.fullName}`
+      ),
+      (r = `'${o.fullName} (${o.name})'`),
+      "string" != typeof o.version &&
+        e(
+          `Failed to register mod ${r}: Expected version to be string, got ${typeof o.version}`
+        ),
+      o.repository || (o.repository = void 0),
+      void 0 !== o.repository &&
+        "string" != typeof o.repository &&
+        e(
+          `Failed to register mod ${r}: Expected repository to be undefined or string, got ${typeof o.version}`
+        ),
+      null == t && (t = {}),
+      (t && "object" == typeof t) ||
+        e(
+          `Failed to register mod ${r}: Expected options to be undefined or object, got ${typeof t}`
+        );
+    const i = !0 === t.allowReplace,
+      a = f.get(o.name);
+    a &&
+      ((a.allowReplace && i) ||
+        e(
+          `Refusing to load mod ${r}: it is already loaded and doesn't allow being replaced.\nWas the mod loaded multiple times?`
+        ),
+      u(a));
+    const c = (o) => {
+        let e = g.patching.get(o.name);
+        return (
+          e ||
+            ((e = { hooks: [], patches: new Map() }),
+            g.patching.set(o.name, e)),
+          e
+        );
+      },
+      s =
+        (o, t) =>
+        (...n) => {
+          var i, a;
+          const c =
+            null === (a = (i = m.errorReporterHooks).apiEndpointEnter) ||
+            void 0 === a
+              ? void 0
+              : a.call(i, o, g.name);
+          g.loaded ||
+            e(`Mod ${r} attempted to call SDK function after being unloaded`);
+          const s = t(...n);
+          return null == c || c(), s;
+        },
+      p = {
+        unload: s("unload", () => u(g)),
+        hookFunction: s("hookFunction", (o, t, n) => {
+          ("string" == typeof o && o) ||
+            e(
+              `Mod ${r} failed to patch a function: Expected function name string, got ${typeof o}`
+            );
+          const i = l(o),
+            a = c(i);
+          "number" != typeof t &&
+            e(
+              `Mod ${r} failed to hook function '${o}': Expected priority number, got ${typeof t}`
+            ),
+            "function" != typeof n &&
+              e(
+                `Mod ${r} failed to hook function '${o}': Expected hook function, got ${typeof n}`
+              );
+          const s = { mod: g.name, priority: t, hook: n };
+          return (
+            a.hooks.push(s),
+            d(),
+            () => {
+              const o = a.hooks.indexOf(s);
+              o >= 0 && (a.hooks.splice(o, 1), d());
+            }
+          );
+        }),
+        patchFunction: s("patchFunction", (o, t) => {
+          ("string" == typeof o && o) ||
+            e(
+              `Mod ${r} failed to patch a function: Expected function name string, got ${typeof o}`
+            );
+          const i = l(o),
+            a = c(i);
+          n(t) ||
+            e(
+              `Mod ${r} failed to patch function '${o}': Expected patches object, got ${typeof t}`
+            );
+          for (const [n, i] of Object.entries(t))
+            "string" == typeof i
+              ? a.patches.set(n, i)
+              : null === i
+              ? a.patches.delete(n)
+              : e(
+                  `Mod ${r} failed to patch function '${o}': Invalid format of patch '${n}'`
+                );
+          d();
+        }),
+        removePatches: s("removePatches", (o) => {
+          ("string" == typeof o && o) ||
+            e(
+              `Mod ${r} failed to patch a function: Expected function name string, got ${typeof o}`
+            );
+          const t = l(o);
+          c(t).patches.clear(), d();
+        }),
+        callOriginal: s("callOriginal", (o, t, n) => {
+          ("string" == typeof o && o) ||
+            e(
+              `Mod ${r} failed to call a function: Expected function name string, got ${typeof o}`
+            );
+          const i = l(o);
+          return (
+            Array.isArray(t) ||
+              e(
+                `Mod ${r} failed to call a function: Expected args array, got ${typeof t}`
+              ),
+            i.original.apply(null != n ? n : globalThis, t)
+          );
+        }),
+        getOriginalHash: s("getOriginalHash", (o) => {
+          ("string" == typeof o && o) ||
+            e(
+              `Mod ${r} failed to get hash: Expected function name string, got ${typeof o}`
+            );
+          return l(o).originalHash;
+        }),
+      },
+      g = {
+        name: o.name,
+        fullName: o.fullName,
+        version: o.version,
+        repository: o.repository,
+        allowReplace: i,
+        api: p,
+        loaded: !0,
+        patching: new Map(),
+      };
+    return f.set(o.name, g), Object.freeze(p);
+  }
+  function h() {
+    const o = [];
+    for (const e of f.values())
+      o.push({
+        name: e.name,
+        fullName: e.fullName,
+        version: e.version,
+        repository: e.repository,
+      });
+    return o;
+  }
+  let m;
+  const y =
+    void 0 === window.bcModSdk
+      ? (window.bcModSdk = (function () {
+          const e = {
+            version: o,
+            apiVersion: 1,
+            registerMod: g,
+            getModsInfo: h,
+            getPatchingInfo: p,
+            errorReporterHooks: Object.seal({
+              apiEndpointEnter: null,
+              hookEnter: null,
+              hookChainExit: null,
+            }),
+          };
+          return (m = e), Object.freeze(e);
+        })())
+      : (n(window.bcModSdk) || e("Failed to init Mod SDK: Name already in use"),
+        1 !== window.bcModSdk.apiVersion &&
+          e(
+            `Failed to init Mod SDK: Different version already loaded ('1.2.0' vs '${window.bcModSdk.version}')`
+          ),
+        window.bcModSdk.version !== o &&
+          alert(
+            `Mod SDK warning: Loading different but compatible versions ('1.2.0' vs '${window.bcModSdk.version}')\nOne of mods you are using is using an old version of SDK. It will work for now but please inform author to update`
+          ),
+        window.bcModSdk);
+  return (
+    "undefined" != typeof exports &&
+      (Object.defineProperty(exports, "__esModule", { value: !0 }),
+      (exports.default = y)),
+    y
+  );
+})();
+
+//#endregion
+
 /*import(
-    `https://lunarkitsunify.github.io/RoomCardDecksEditorBC/MoonCEBC.js?v=${(
-      Date.now() / 10000
-    ).toFixed(0)}`
-  );*/
+      `https://lunarkitsunify.github.io/MoonCEBC/MoonCEBC.js?v=${(
+        Date.now() / 10000
+      ).toFixed(0)}`
+    );*/
 
 (function () {
   "use strict";
@@ -29,6 +386,7 @@
   const AddonName = "Moon Cards Editor BC";
   let isVisibleMainWindow = false;
   const cells = [];
+  const w = window;
 
   //#region Size and color customization for the card rendering
 
@@ -38,6 +396,34 @@
   const cardNameFontSize = "60%";
   const cardGroupFontSize = "55%";
   const cardTextFontSize = "50%";
+  //#endregion
+
+  //#region BC Mod SDK Stuff
+
+  const modApi = bcModSdk.registerMod({
+    name: "MoonCEBC",
+    fullName: "Moon Cards Editor BC",
+    version: "1.0.0",
+    repository: "https://github.com/LunarKitsunify/RoomCardDecksEditorBC",
+  });
+
+  modApi.hookFunction("ClubCardLoad", 0, (args, next) => {
+    console.log("TestHook ClubCardLoad");
+    next();
+  });
+
+  modApi.hookFunction("ClubCardRenderCard", 0, (args, next) => {
+    console.log("TestHook ClubCardRenderCard");
+    next(args);
+  });
+
+  modApi.hookFunction("MainRun", 0, (args, next) => {
+    //TODO Hook ChatRoomRun and do it with a DrawButton?
+    //This should minimize the load on the server instead of constantly running in MainRun
+    next(args);
+    UpdateStatusShowButton();
+  });
+
   //#endregion
 
   //#region UI
@@ -151,6 +537,9 @@
     cardCell.style.justifyContent = "center";
     cardCell.style.alignItems = "center";
     cardCell.style.display = "inline-block";
+    cardCell.addEventListener("onclick", () => {
+      console.log("Click");
+    });
     //cardCell.style.background = "blue";
     cells.push(cardCell);
     bottomPanel.appendChild(cardCell);
@@ -167,16 +556,20 @@
     console.log(`${AddonName} Start Load`);
     CardTextContent = new TextCache(CardTextPath); //Load Cards data from BC Server
     console.log(`${AddonName} Load Complete`);
-
-    setInterval(UpdateStatusShowButton, 500);
   }
 
   function UpdateStatusShowButton() {
-    const isClubCardsGame = ChatRoomGame;
-    const isInChatRoom = ServerPlayerIsInChatRoom();
-    if (isInChatRoom && isClubCardsGame == "ClubCard")
+    //check if in room selected ClubCard game
+    const isClubCardsGame = ChatRoomGame == "ClubCard";
+    //check where the player is
+    const isInChatRoom = CurrentScreen == "ChatRoom";
+
+    const isShowButton = isInChatRoom && isClubCardsGame;
+
+    if (isShowButton && showButton.style.display !== "block")
       showButton.style.display = "block";
-    else showButton.style.display = "none";
+    else if (!isShowButton && showButton.style.display !== "none")
+      showButton.style.display = "none";
   }
 
   function LoadPlayerData() {
@@ -289,9 +682,6 @@
    * @returns {void} - Nothing
    */
   function DrawCard(Card, cardCell) {
-    const img = document.createElement("img");
-    const imgFrame = document.createElement("img");
-    const backgroundContainer = document.createElement("div");
     let Level =
       Card.RequiredLevel == null || Card.RequiredLevel <= 1
         ? 1
@@ -301,6 +691,8 @@
 
     //#region Background
 
+    //const backgroundContainer = document.createElement("div");
+    const backgroundContainer = document.createElement("button");
     backgroundContainer.style.position = "relative";
     backgroundContainer.style.overflow = "hidden";
     backgroundContainer.style.margin = "0 auto";
@@ -308,18 +700,23 @@
     backgroundContainer.style.visibility = "hidden";
     backgroundContainer.style.display = "inline - block";
     backgroundContainer.style.justifyContent = "center";
+    backgroundContainer.style.userSelect = "none";
+    //backgroundContainer.style.pointerEvents = "none";
+    //backgroundContainer.style.zIndex = "10";
+    //TODO dont work
+    backgroundContainer.addEventListener("click", () => {
+      console.log("Click");
+    });
+    backgroundContainer.addEventListener("onmouseover", () => {
+      backgroundContainer.style.border = "1px solid green";
+      console.log("Over");
+    });
+    backgroundContainer.addEventListener("onmouseleave ", () => {
+      backgroundContainer.style.border = "none";
+      console.log("Leave");
+    });
 
-    img.src =
-      "Screens/MiniGame/ClubCard/" + Card.Type + "/" + Card.Name + ".png";
-    img.style.height = "78%";
-    img.style.position = "absolute";
-    img.style.top = "22%";
-    img.style.maxWidth = "100%";
-    img.style.maxHeight = "100%";
-    img.style.objectFit = "contain";
-    img.style.left = "50%";
-    img.style.transform = "translateX(-50%)";
-
+    const imgFrame = document.createElement("img");
     imgFrame.src =
       "Screens/MiniGame/ClubCard/Frame/" +
       Card.Type +
@@ -331,24 +728,30 @@
     imgFrame.style.position = "absolute";
     imgFrame.style.objectFit = "contain";
     imgFrame.style.left = "50%";
+    imgFrame.style.top = "0";
     imgFrame.style.transform = "translateX(-50%)";
     imgFrame.style.display = "block";
-    imgFrame.onload = function () {
+    imgFrame.addEventListener("load", () => {
       //TODO I really don't like this implementation.
       //Sometimes it slows down and does not render files.
       //And also it doesn't update when the browser window size changes.
       const imgWidth = imgFrame.offsetWidth;
       backgroundContainer.style.width = `${imgWidth}px`;
       backgroundContainer.style.visibility = "visible";
-    };
-
-    /*imgFrame.addEventListener('load', () => {
-            const imgWidth = imgFrame.offsetWidth;
-            backgroundContainer.style.width = `${imgWidth}px`;
-            backgroundContainer.style.visibility = "visible";
-          });*/
-
+    });
     backgroundContainer.appendChild(imgFrame);
+
+    const img = document.createElement("img");
+    img.src =
+      "Screens/MiniGame/ClubCard/" + Card.Type + "/" + Card.Name + ".png";
+    img.style.height = "78%";
+    img.style.position = "absolute";
+    img.style.top = "22%";
+    img.style.maxWidth = "100%";
+    img.style.maxHeight = "100%";
+    img.style.objectFit = "contain";
+    img.style.left = "50%";
+    img.style.transform = "translateX(-50%)";
     backgroundContainer.appendChild(img);
 
     //#endregion
