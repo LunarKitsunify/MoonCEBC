ab4d9b79c42465629743c3ac177591234e506d71
LunarKitsunify <kitsunify@gmail.com>
2024-11-28 21:26:03 +0600

    Update beta

--- MoonCEBCLoaderBeta.user.js
+++ MoonCEBCLoaderBeta.user.js
@@ -33,7 +33,7 @@
   var script = document.createElement("script");
   script.type = 'module';
   script.setAttribute("crossorigin", "anonymous");
-  script.src = "https://lunarkitsunify.github.io/MoonCEBC/MoonCEBCBeta.js";
+  script.src = "http://127.0.0.1:5500/MoonCEBCBeta.js";
   document.head.appendChild(script);
 })();
 
