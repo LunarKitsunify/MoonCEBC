25ddc9cc959f7d7e1d5f7b2094a24ddfbd9307e7
LunarKitsunify <kitsunify@gmail.com>
2025-05-19 23:16:17 +0600

    !

--- entry.js
+++
@@ -1,9 +0,0 @@
-// entry.js
-import './Style/styles.css';
-import './Style/cardRender.css';
-
-import { createCard, createGridLayout } from './RenderObjs/CardRender.js';
-import { createModal, createSettingsMenu } from './RenderObjs/SettingsMenu.js';
-import { bcModSdk } from './src/BCModSdk.js';
-
-console.log("✅ MoonCEBC bundled and ready.");
